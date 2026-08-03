c334d92972b967b65258fda00226a8dabb37401e
mikolajpioro <mytocpioro@gmail.com>
2026-08-03 22:02:41 +0200

    Add account page and update navigation: implement account page template and modify navbar for user account access

--- main.py
+++ main.py
@@ -107,6 +107,10 @@ async def register_page(request: Request):
     return templates.TemplateResponse(request, "register.html", {"title": "Register"})
 # Register and Login -----------------
 
+@app.get("/account", include_in_schema=False)
+async def account_page(request:Request):
+    return templates.TemplateResponse(request, "account.html", {"title": "Account"})
+
 #error routes------------------
 @app.exception_handler(StarletteHTTPException)
 async def general_https_exception_handler(request: Request, exception: StarletteHTTPException):
